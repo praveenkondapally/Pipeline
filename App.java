d5845ee2fc74993630267a64864369f6736dac51
praveenkondapally <43089438+praveenkondapally@users.noreply.github.com>
2026-08-28 05:14:56 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println(" Java Maven Jenkins Application ");
+        System.out.println("================================");
+
+        System.out.println("Application deployed successfully!");
+
+    }
+}
